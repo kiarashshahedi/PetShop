72245b0e7ce7cb287829302588ce778830876ecc
Kiarash <shahedikiarash@gmail.com>
2024-06-02 21:23:32 +0330

    fixing home page products boxes and adding some products to database

--- petshop/products/views.py
+++ petshop/products/views.py
@@ -3,8 +3,30 @@ from .models import MainCategory, Product, SubCategory, AnimalType
 
 # Home Page List Of Products
 def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/home_products_list.html', {'products': products})
+    animal_types_with_subcategories = []
+    animal_types = AnimalType.objects.all()
+
+    for animal_type in animal_types:
+        subcategories_with_products = []
+        main_categories = MainCategory.objects.filter(animal_type=animal_type)[:2]  # محدود کردن به 2 main category
+        for main_category in main_categories:
+            subcategories = SubCategory.objects.filter(main_category=main_category)[:1]  # محدود کردن به 1 sub category
+            for subcategory in subcategories:
+                products = Product.objects.filter(sub_category=subcategory)[:10]  # محدود کردن تعداد محصولات به 10
+                subcategories_with_products.append({
+                    'sub_category': subcategory,
+                    'products': products
+                })
+        if subcategories_with_products:
+            animal_types_with_subcategories.append({
+                'animal_type': animal_type,
+                'subcategories_with_products': subcategories_with_products
+            })
+    
+    return render(request, 'products/home_products_list.html', {
+        'animal_types_with_subcategories': animal_types_with_subcategories,
+    })
+
 
 # Products Detail pages 
 def product_detail(request, id):
