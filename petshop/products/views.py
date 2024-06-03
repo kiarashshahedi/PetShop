617ed9e3d409b5781323ee8496d060dfb1acbdd4
Kiarash <shahedikiarash@gmail.com>
2024-06-03 18:07:47 +0330

    add smt to home page

--- petshop/products/views.py
+++ petshop/products/views.py
@@ -9,8 +9,9 @@ def product_list(request):
     for animal_type in animal_types:
         subcategories_with_products = []
         main_categories = MainCategory.objects.filter(animal_type=animal_type)[:2]  # Limit to 2 main categories
+        
         for main_category in main_categories:
-            subcategories = SubCategory.objects.filter(main_category=main_category)[:1]  # Limit to 1 subcategory
+            subcategories = SubCategory.objects.filter(main_category=main_category)[:2]  # Limit to 1 subcategory
             for subcategory in subcategories:
                 products = Product.objects.filter(sub_category=subcategory)[:10]  # Limit products to 10
                 slider_images = get_static_slider_images()  # Fetch static slider images
@@ -19,6 +20,7 @@ def product_list(request):
                     'products': products,
                     'slider_images': slider_images  # Add static slider images to the context
                 })
+                
         if subcategories_with_products:
             animal_types_with_subcategories.append({
                 'animal_type': animal_type,
@@ -27,6 +29,7 @@ def product_list(request):
     
     return render(request, 'products/home_products_list.html', {
         'animal_types_with_subcategories': animal_types_with_subcategories,
+
     })
 
 def get_static_slider_images():
@@ -61,3 +64,5 @@ def sub_category_view(request, sub_category_id):
     sub_category = get_object_or_404(SubCategory, id=sub_category_id)
     products = Product.objects.filter(sub_category=sub_category)
     return render(request, 'products/sub_category.html', {'sub_category': sub_category, 'products': products})
+
+
